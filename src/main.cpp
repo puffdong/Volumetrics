999d8af2a53ce950bab0a2a393893bd5d0f4fe5a
Puff <puff@macbookpro.students.netlogon.liu.se>
2025-10-20 17:48:41 +0200

    cmake file made more neat

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,32 @@
     #define ASSET_PATH "C:/Dev/OpenGL/Volumetrics/res/"
 #endif 
 
+void print_cpp_version() {
+    std::cout << "C++ Version: "; // __cplusplus
+#if   __cplusplus >= 202302L
+    std::cout << "C++23 or later" << std::endl;
+#elif __cplusplus >= 202002L
+    std::cout << "C++20" << std::endl;
+#elif __cplusplus >= 201703L
+    std::cout << "C++17" << std::endl;
+#elif __cplusplus >= 201402L
+    std::cout << "C++14" << std::endl;
+#elif __cplusplus >= 201103L
+    std::cout << "C++11" << std::endl;
+#else
+    std::cout << "Pre-C++11 (C++98/03)" << std::endl;
+#endif
+}
+
 int main(void)
 {
-#ifdef __APPLE__ // this looks like doodoo, but who else is using this except me right now?
-    AppConfig config{650, 400, ASSET_PATH};
+    print_cpp_version();
+#ifdef __APPLE__
+    AppConfig config{650, 400, ASSET_PATH, "macOS"};
 #endif 
 #if defined _WIN32 || defined _WIN64
-    AppConfig config{1920, 1080, ASSET_PATH};
-#endif 
+    AppConfig config{1920, 1080, ASSET_PATH, "Windows"};
+#endif
     Application application(config);
     return application.run();
 }
