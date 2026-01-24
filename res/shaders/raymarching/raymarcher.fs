256bcbb469939f976d0a6b88c18f66682f8872b8
Simon Magnusson <simma925@student.liu.se>
2026-01-24 05:36:40 +0100

    the smoke looks drastically better now

--- res/shaders/raymarching/raymarcher.fs
+++ res/shaders/raymarching/raymarcher.fs
@@ -93,31 +93,31 @@ float henyey_greenstein(float cos_theta, float g) {
 
 float sample_density(vec3 sample_pos) {
     float noise = texture(u_noise_texture, (sample_pos) * 0.05).r;
+    noise = smoothstep(0.1, 0.8, noise);
+       
     return noise;
 }
 
 float do_light_march(vec3 light_pos, vec3 light_dir) {
     float distance_traveled = 0.0;
-    float total_density = 0.0;
+    float optical_depth = 0.0;
+
+    float sigma_t = u_scattering_coefficient + u_absorption_coefficient;
 
     for (int i = 0; i < u_max_light_steps; ++i) {
         vec3 ray_pos = light_pos + light_dir * distance_traveled;
-        uint v = get_voxel(ray_pos); // snaps to closest voxel
 
+        // If you want volumetric even outside voxels, sample anyway.
+        uint v = get_voxel(ray_pos);
         if (v != 0u) {
             float density = sample_density(ray_pos);
-            total_density += density * u_light_step_size;
-            if (total_density > 1.0) {
-                total_density = 1.0;
-                break;
-            }
+            optical_depth += density * sigma_t * u_light_step_size;
         }
 
         distance_traveled += u_light_step_size;
     }
-    
-    float transmittance = exp(-total_density * u_scattering_coefficient * u_absorption_coefficient);
-    return transmittance;
+
+    return exp(-optical_depth);
 }
 
 vec4 do_raymarch(vec3 ray_origin, vec3 ray_direction, float scene_depth, float raymarch_depth) {    
@@ -127,17 +127,28 @@ vec4 do_raymarch(vec3 ray_origin, vec3 ray_direction, float scene_depth, float r
     float distance_traveled = start_dist;
 
     float transmittance = 1.0;
-    float light_energy = 0.0;
+    vec3 light_energy = vec3(0.0);
+
+    float sigma_s = u_scattering_coefficient;
+    float sigma_t = u_scattering_coefficient + u_absorption_coefficient;
 
     vec3 sun_vec = normalize(u_sun_dir);
-
     float cos_theta = dot(sun_vec, ray_direction);
     float phase = henyey_greenstein(cos_theta, u_anisotropy);
 
     float collected_density = 0.0;
 
-    Light point_light = u_lights[0];
-    
+    Light point_light1 = u_lights[0];
+    Light point_light2 = u_lights[1];
+
+    // unpack point lights
+    vec3 point_light_pos = point_light1.position_radius.xyz;
+    vec3 point_light_color = point_light1.color_intensity.xyz;
+    float point_light_intensity = point_light1.color_intensity.w;
+    vec3 point_light2_pos = point_light2.position_radius.xyz;
+    vec3 point_light2_color = point_light2.color_intensity.xyz;
+    float point_light2_intensity = point_light2.color_intensity.w;
+
     for (int i = 0; i < u_max_steps; ++i) {
         
         vec3 ray_pos = ray_origin + ray_direction * distance_traveled;
@@ -145,26 +156,26 @@ vec4 do_raymarch(vec3 ray_origin, vec3 ray_direction, float scene_depth, float r
 
         if (v != 0u) {
             float density = sample_density(ray_pos);
+
             collected_density += density * u_step_size;
 
-            vec3 light_ray_pos = ray_pos;
-            vec3 light_result_color = vec3(0.0);
+            float light_transmittance1 = do_light_march(ray_pos, sun_vec);
+            float light_transmittance2 = do_light_march(ray_pos, normalize(point_light_pos - ray_pos));
+            float light_transmittance3 = do_light_march(ray_pos, normalize(point_light2_pos - ray_pos));
+            vec3 Li = u_sun_color * u_sun_intensity * light_transmittance1;
+            
+            Li += point_light_color * point_light_intensity * light_transmittance2;
+            Li += point_light2_color * point_light2_intensity * light_transmittance3;
 
-            float light_transmittance = do_light_march(light_ray_pos, normalize(u_sun_dir));
-            float incoming_light = phase * light_transmittance;
+            light_energy += transmittance * density * sigma_s * phase * Li * u_step_size;
 
-            light_energy += density * u_step_size * transmittance * light_transmittance;
-            transmittance *= exp(-density * u_step_size * u_scattering_coefficient);
+            transmittance *= exp(-density * sigma_t * u_step_size);
 
             if (transmittance < 0.01) {
                 transmittance = 0.0;
                 break;
             }
 
-            if (collected_density >= 1.0) {
-                collected_density = 1.0;
-                break;
-            }
         }
 
         if (distance_traveled > scene_dist) break; // hmm maybe this can be used to determine max steps dynamically?
@@ -173,7 +184,7 @@ vec4 do_raymarch(vec3 ray_origin, vec3 ray_direction, float scene_depth, float r
 
     vec3 ambient = u_base_color;
     vec3 cloud_color = vec3(light_energy);
-    vec3 final_color = ambient * transmittance + cloud_color;
+    vec3 final_color = ambient * transmittance + light_energy;
 
     return vec4(final_color, clamp(collected_density, 0.0, 1.0));
 }
